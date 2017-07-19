ebf328fea501cad5779b2a23b217f92d93a66880
Rudys Natanael Acosta crousset <natanael926@gmail.com>
2017-07-18 20:38:58 -0400

    Agregar servidor en webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 var pkg = require('./package.json');
 
 module.exports = {
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  context: __dirname,
-  entry: {
-    app: ['./app.jsx']
-  },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    context: __dirname,
+    entry: {
+        app: ['./app.jsx']
+    },
     devServer: {
         host: '127.0.0.1',
         port: 8080,
         inline: true
     },
-  output: {
-    path: './build',
-    filename: pkg.name + '.js',
-    publicPath: '/build/'
-  },
-  module: {
-    loaders: [
-      {
-        test: /(\.js|.jsx)$/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'stage-2', 'react']
+    output: {
+        path: './build',
+        filename: pkg.name + '.js',
+        publicPath: '/build/'
+    },
+    module: {
+        loaders: [
+        {
+            test: /(\.js|.jsx)$/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'stage-2', 'react']
+            }
         }
-      }
     ]
   }
 }
